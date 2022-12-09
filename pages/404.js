a3e491806f9dbba91c928633a3010b96de26867e
Alok <alok@Aloks-MacBook-Air.local>
2022-12-09 19:33:25 +0600

    added 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Error = () => {
+    return (
+        <div>
+            <Image src='/error.webp' width={1440} height={100} alt=''></Image>
+        </div>
+    );
+};
+
+export default Error;
